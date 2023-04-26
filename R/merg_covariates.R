d68832e2f60b63c1465c6326c2b075002752f146
Jeremiah J. Nieves <jjniev01@users.noreply.github.com>
2023-04-26 10:01:15 +0100

    Fixed spelling, line lengths, and spacings

--- R/merg_covariates.R
+++ R/merg_covariates.R
@@ -2,14 +2,14 @@
 #'
 #' @param input.countries list countries 
 #' @param covariates.var.names list of covariates names 
-#' @param covariates detailes of the covariates
+#' @param covariates details of the covariates
 #' @param rfg.countries.tag tag for the project
 #' @param rfg.countries.merged directory to save merged rasters
 #' @param verbose is logical. TRUE or FALSE: flag indicating whether to print 
 #'        intermediate output from the function on the console, which might be 
 #'        helpful for model debugging. Default is \code{verbose} = TRUE.
-#' @param log is logical. TRUE or FALSE: flag indicating whether to print intermediate 
-#'        output from the function on the log.txt file. 
+#' @param log is logical. TRUE or FALSE: flag indicating whether to print 
+#'        intermediate output from the function on the log.txt file. 
 #'        Default is \code{log} = FALSE.
 #' @importFrom terra
 #' @noRd 
@@ -33,7 +33,10 @@ merge_covariates <- function(input.countries,
   
   rfg.input.countries <- input.countries
   
-  log_info("MSG", paste0("Start merging covariates"), verbose=verbose, log=log) 
+  log_info("MSG",
+           paste0("Start merging covariates"),
+           verbose=verbose,
+           log=log) 
   
   covariates_merged <- list()
   
@@ -52,7 +55,11 @@ merge_covariates <- function(input.countries,
       
     }
     
-    dstfile <- file.path(rfg.countries.merged, paste0(rfg.countries.tag, "_",cname,".tif"))
+    dstfile <- file.path(rfg.countries.merged,
+                         paste0(rfg.countries.tag,
+                                "_",
+                                cname,
+                                ".tif"))
     
     if (!file.exists(dstfile)){
       
@@ -60,7 +67,8 @@ merge_covariates <- function(input.countries,
       # gdalwarp(srcfile=list_of_tiffs,
       #          of = "GTiff",
       #          dstfile=dstfile,
-      #          co=c("COMPRESS=LZW", "BLOCKXSIZE=512", "BLOCKYSIZE=512", "TILED=YES", "BIGTIFF=YES"),
+      #          co=c("COMPRESS=LZW", "BLOCKXSIZE=512",
+      #           "BLOCKYSIZE=512", "TILED=YES", "BIGTIFF=YES"),
       #          s_srs=crs(raster(rst)),
       #          output_Raster=TRUE,
       #          overwrite=TRUE,
@@ -84,12 +92,12 @@ merge_covariates <- function(input.countries,
     
     #covariates_merged[[rfg.countries.tag]][[cname]][["path"]] <- dstfile
     covariates_merged[[rfg.countries.tag]][[cname]] <- list(dataset_folder = dirname(dstfile), 
-                                                            dataset_filename    = basename(dstfile),
+                                                            dataset_filename = basename(dstfile),
                                                             dataset_description = cname,
-                                                            dataset_summary     = rtype,
-                                                            dataset_country     = cname,
-                                                            dataset_class       = cname,
-                                                            dataset_path        = dstfile 
+                                                            dataset_summary = rtype,
+                                                            dataset_country = cname,
+                                                            dataset_class = cname,
+                                                            dataset_path  = dstfile 
     )
     
     
@@ -98,13 +106,13 @@ merge_covariates <- function(input.countries,
         
         progress_message(x=i, 
                          max=length(covariates.var.names), 
-                         label=paste0("Merging ",cname))  
+                         label=paste0("Merging ", cname))  
         
       }else{
         
         progress_message(x=i, 
                          max=length(covariates.var.names), 
-                         label=paste0("Complited merging                         " ))
+                         label=paste0("Merging complete                     " ))
         
       }
     }      
@@ -113,7 +121,8 @@ merge_covariates <- function(input.countries,
     
   } 
   
-  #log_info("MSG", paste0("Completed merging covariates"), verbose=verbose, log=log) 
+  #log_info("MSG", paste0("Completed merging covariates"),
+  # verbose=verbose, log=log) 
   return(covariates_merged)
   
 }
